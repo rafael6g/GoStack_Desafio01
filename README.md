1b8cdaed2ed60c2e57b0764e4f7157452e393649
Rafael C Fernandes <rafael.londrina@gmail.com>
2020-08-05 16:07:21 -0300

    GoStack desafio 1

---
+++ README.md
@@ -0,0 +1,27 @@
+<img alt="GoStack" src="https://storage.googleapis.com/golden-wind/bootcamp-gostack/header-desafios.png" />
+
+<h3 align="center">
+  Desafio 02: Conceitos do Node.js
+</h3>
+
+<blockquote align="center">“Não espere para plantar, apenas tenha paciência para colher”!</blockquote>
+
+<p align="center">
+  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/rocketseat/bootcamp-gostack-desafios?color=%2304D361">
+
+  <a href="https://rocketseat.com.br">
+    <img alt="Made by Rocketseat" src="https://img.shields.io/badge/made%20by-Rocketseat-%2304D361">
+  </a>
+
+  <img alt="License" src="https://img.shields.io/badge/license-MIT-%2304D361">
+
+  <a href="https://github.com/Rocketseat/bootcamp-gostack-desafios/stargazers">
+    <img alt="Stargazers" src="https://img.shields.io/github/stars/rocketseat/bootcamp-gostack-desafios?style=social">
+  </a>
+</p>
+
+
+
+## :rocket: Sobre o desafio
+
+Meu codigo, passou pelo Jest, mas ele esta errado!
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -9,25 +9,116 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+const techs = [];
+
+function logRequest(request, response, next) {
+  const { method, url, status } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url} `;
+
+  console.log(logLabel);
+
+  return next();
+};
+
+function validadeRepositoryId(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID.'})
+  }
+
+  return next()
+}
+
+app.use(logRequest);
+app.use('/repositories/:id', validadeRepositoryId); //aplica para todos
+
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  const { title } = request.query;
+
+  const results = title
+    ? repositories.filter(repository => repository.title.includes(title))
+    : repositories;
+
+  return response.json(results);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+
+  return response.json(repository); //retornar apenas o projeto criado e nao a lista
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const { likes } = request.query;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id); //buscar a posiçao do projeto fica mais facil
+
+    if (repositoryIndex < 0 ) {
+      return response.status(400).json({ "error": "Repository not found." });
+    }
+
+    const like = repositories[repositoryIndex].likes;
+
+    const repository = {
+      id,
+      title,
+      url,
+      techs,
+      likes:like,
+    };
+
+    repositories[repositoryIndex] = repository;
+    
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id); //buscar a posiçao do projeto fica mais facil
+
+    if (repositoryIndex < 0 ) {
+      return response.status(400).json({ "error": "Repository not found." });
+    }
+
+    repositories.splice(repositoryIndex, 1);
+
+    
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.query;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id); //buscar a posiçao do projeto fica mais facil
+
+    if (repositoryIndex < 0 ) {
+      return response.status(400).json({ "error": "Repository not found." });
+    }
+
+    const like = repositories[repositoryIndex].likes + 1;
+
+    const repository = {
+      id,
+      title,
+      url,
+      techs,
+      likes:like,
+    };
+
+    repositories[repositoryIndex] = repository;
+    
+  return response.json(repository);
 });
 
 module.exports = app;
